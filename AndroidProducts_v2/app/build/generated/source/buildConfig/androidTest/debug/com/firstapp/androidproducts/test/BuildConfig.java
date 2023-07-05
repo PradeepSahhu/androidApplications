805af4e929894b8909945954148fe886f96782dc
Pradeep Sahu <official.pradeepsahu@gmail.com>
2023-07-05 19:26:30 +0530

    updated file

---
+++ AndroidProducts_v2/app/build/generated/source/buildConfig/androidTest/debug/com/firstapp/androidproducts/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.firstapp.androidproducts.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.firstapp.androidproducts.test";
+  public static final String BUILD_TYPE = "debug";
+}
